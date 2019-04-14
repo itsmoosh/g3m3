2a8587e5794bc13a903eb1b999e8937740edf268
Marshall Styczinski <itsmoosh@gmail.com>
2019-04-13 19:23:23 -0700

    Formatting updates, missed in s_set_rho

--- code/s_set_rho.f90
+++ code/s_set_rho.f90
@@ -37,40 +37,47 @@ subroutine set_rho(qrho,qpresx,qpresy,qpresz, &
 		do  j=1,ny
 			do  i=1,nx
 		
-				tden=abs(qrho(i,j,k,box))/rmassq+abs(hrho(i,j,k,box))/rmassh &
-				+abs(orho(i,j,k,box))/rmasso+1.e-6
-				rhdens=abs(hrho(i,j,k,box))/rmassh/tden
-				rodens=abs(orho(i,j,k,box))/rmasso/tden
-				!
-				if ((qrho(i,j,k,box).lt.q_min).or.(qpresx(i,j,k,box).lt.0.).or. &
-				(qpresy(i,j,k,box).lt.0.).or.(qpresz(i,j,k,box).lt.0.))then
-				!
-				qrho(i,j,k,box)=amax1(abs(qrho(i,j,k,box)),1.05*q_min)
-				apres=(abs(qpresx(i,j,k,box))+abs(qpresy(i,j,k,box))+ &
-				abs(qpresz(i,j,k,box)))/3.
-				qpresx(i,j,k,box)=apres
-				qpresy(i,j,k,box)=apres
-				qpresz(i,j,k,box)=apres
-				qpresxy(i,j,k,box)=0.
-				qpresxz(i,j,k,box)=0.
-				qpresyz(i,j,k,box)=0.
+				tden = abs( qrho(i,j,k,box) )/rmassq &
+					+ abs( hrho(i,j,k,box) ) /rmassh &
+					+ abs( orho(i,j,k,box) )/rmasso + 1.e-6
+				rhdens = abs( hrho(i,j,k,box) ) /rmassh/tden
+				rodens = abs( orho(i,j,k,box) ) /rmasso/tden
+				
+				if( (qrho(i,j,k,box).lt.q_min) &
+					.or. (qpresx(i,j,k,box).lt.0.) &
+					.or. (qpresy(i,j,k,box).lt.0.) &
+					.or. (qpresz(i,j,k,box).lt.0.) ) then
+				
+					qrho(i,j,k,box)=amax1( abs(qrho(i,j,k,box)),1.05*q_min )
+					apres = ( abs(qpresx(i,j,k,box)) &
+						+ abs(qpresy(i,j,k,box)) &
+						+ abs(qpresz(i,j,k,box)) )/3.
+					qpresx(i,j,k,box)=apres
+					qpresy(i,j,k,box)=apres
+					qpresz(i,j,k,box)=apres
+					qpresxy(i,j,k,box)=0.
+					qpresxz(i,j,k,box)=0.
+					qpresyz(i,j,k,box)=0.
 				endif
-				!
-				if ((hrho(i,j,k,box).lt.h_min).or.(hpresx(i,j,k,box).lt.0.).or. &
-				(hpresy(i,j,k,box).lt.0.).or.(hpresz(i,j,k,box).lt.0.))then
+				
+				if( (hrho(i,j,k,box).lt.h_min) &
+					.or. (hpresx(i,j,k,box).lt.0.) &
+					.or. (hpresy(i,j,k,box).lt.0.) &
+					.or. (hpresz(i,j,k,box).lt.0.) ) then
 
-				hrho(i,j,k,box)=amax1(abs(hrho(i,j,k,box)),1.05*h_min)
-				apres=(abs(hpresx(i,j,k,box))+abs(hpresy(i,j,k,box))+ &
-				abs(hpresz(i,j,k,box)))/3.
-				hpresx(i,j,k,box)=apres
-				hpresy(i,j,k,box)=apres
-				hpresz(i,j,k,box)=apres
-				hpresxy(i,j,k,box)=0.
-				hpresxz(i,j,k,box)=0.
-				hpresyz(i,j,k,box)=0.
+					hrho(i,j,k,box)=amax1( abs(hrho(i,j,k,box)),1.05*h_min )
+					apres = ( abs(hpresx(i,j,k,box)) &
+						+ abs(hpresy(i,j,k,box)) &
+						+ abs(hpresz(i,j,k,box)) )/3.
+					hpresx(i,j,k,box)=apres
+					hpresy(i,j,k,box)=apres
+					hpresz(i,j,k,box)=apres
+					hpresxy(i,j,k,box)=0.
+					hpresxz(i,j,k,box)=0.
+					hpresyz(i,j,k,box)=0.
 				endif
-				!
-				if ( (orho(i,j,k,box).lt.o_min) &
+				
+				if( (orho(i,j,k,box).lt.o_min) &
 					.or. (opresx(i,j,k,box).lt.0.) &
 					.or. (opresy(i,j,k,box).lt.0.) &
 					.or.(opresz(i,j,k,box).lt.0.) ) then
